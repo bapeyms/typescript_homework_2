b672191c0b2458357147b75758ca17c1c086ca46
Katherine <katerynaunik@gmail.com>
2026-04-24 02:11:51 +0300

    task 2

--- pr.js
+++ pr.js
@@ -58,3 +58,118 @@ console.log(`Roots for equation 1: ${result1}\nRoots for equation 2: ${result2}\
 console.log("TASK 2");
 class Shape {
 }
+class Square extends Shape {
+    _x;
+    _y;
+    _side;
+    constructor(x, y, side) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._side = side;
+    }
+    show() {
+        console.log(`Square:\n- Coordinates: ${this._x}, ${this._y}\n- Side: ${this._side}\n`);
+    }
+    save() {
+        return `(${this._x},${this._y}),${this._side}`;
+    }
+    load(data) {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._side = Number(parts[2]);
+        console.log('Square data has been loaded!\n');
+    }
+}
+class Rectangle extends Shape {
+    _x;
+    _y;
+    _width;
+    _height;
+    constructor(x, y, width, height) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+    }
+    show() {
+        console.log(`Rectangle:\n- Coordinates: ${this._x}, ${this._y}\n- Width: ${this._width}\n- Height: ${this._height}\n`);
+    }
+    save() {
+        return `(${this._x},${this._y}),${this._width},${this._height}`;
+    }
+    load(data) {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._width = Number(parts[2]);
+        this._height = Number(parts[3]);
+        console.log('Rectangle data has been loaded!\n');
+    }
+}
+class Circle extends Shape {
+    _x;
+    _y;
+    _radius;
+    constructor(x, y, radius) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._radius = radius;
+    }
+    show() {
+        console.log(`Circle:\n- Coordinates: ${this._x}, ${this._y}\n- Radius: ${this._radius}\n`);
+    }
+    save() {
+        return `(${this._x},${this._y}),${this._radius}`;
+    }
+    load(data) {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._radius = Number(parts[2]);
+        console.log('Circle data has been loaded!\n');
+    }
+}
+class Ellipse extends Shape {
+    _x;
+    _y;
+    _width;
+    _height;
+    constructor(x, y, width, height) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+    }
+    show() {
+        console.log(`Ellipse:\n- Coordinates: ${this._x}, ${this._y}\n- Dimension: ${this._width} x ${this._height}\n`);
+    }
+    save() {
+        return `(${this._x},${this._y}),${this._width},${this._height}`;
+    }
+    load(data) {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._width = Number(parts[2]);
+        this._height = Number(parts[3]);
+        console.log('Ellipse data has been loaded!\n');
+    }
+}
+const shapes = [
+    new Square(10, 10, 50),
+    new Rectangle(0, 0, 100, 70),
+    new Circle(20, 30, 40),
+    new Ellipse(20, 20, 80, 60)
+];
+shapes.forEach((shape, index) => {
+    console.log(`Shape #${index + 1}: `);
+    shape.show();
+});
+let data = shapes[1].save();
+shapes[1].load("1, 2, 3, 4");
+shapes[1].show();
--- pr.ts
+++ pr.ts
@@ -66,4 +66,131 @@ console.log(`Roots for equation 1: ${result1}\nRoots for equation 2: ${result2}\
 console.log("TASK 2");
 abstract class Shape {
     abstract show(): void;
+    abstract save() : string;
+    abstract load(data: string): void;
 }
+
+class Square extends Shape {
+    private _x: number;
+    private _y: number;
+    private _side: number;
+    constructor(x: number, y: number, side: number) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._side = side;
+    }
+
+    show(): void {
+        console.log(`Square:\n- Coordinates: ${this._x}, ${this._y}\n- Side: ${this._side}\n`);
+    }
+    save() : string {
+        return `(${this._x},${this._y}),${this._side}`;
+    }
+    load(data: string): void {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._side = Number(parts[2]);
+        console.log('Square data has been loaded!\n');
+    }
+}
+
+class Rectangle extends Shape {
+    private _x: number;
+    private _y: number;
+    private _width: number;
+    private _height: number;
+    constructor(x: number, y: number, width: number, height: number) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+    }
+
+    show(): void {
+        console.log(`Rectangle:\n- Coordinates: ${this._x}, ${this._y}\n- Width: ${this._width}\n- Height: ${this._height}\n`);
+    }
+    save() : string {
+        return `(${this._x},${this._y}),${this._width},${this._height}`;
+    }
+    load(data: string): void {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._width = Number(parts[2]);
+        this._height = Number(parts[3]);
+        console.log('Rectangle data has been loaded!\n');
+    }
+}
+
+class Circle extends Shape {
+    private _x: number;
+    private _y: number;
+    private _radius: number;
+    constructor(x: number, y: number, radius: number) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._radius = radius;
+    }
+
+    show(): void {
+        console.log(`Circle:\n- Coordinates: ${this._x}, ${this._y}\n- Radius: ${this._radius}\n`);
+    }
+    save() : string {
+        return `(${this._x},${this._y}),${this._radius}`;
+    }
+    load(data: string): void {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._radius = Number(parts[2]);
+        console.log('Circle data has been loaded!\n');
+    }
+}
+
+class Ellipse extends Shape {
+    private _x: number;
+    private _y: number;
+    private _width: number;
+    private _height: number;
+    constructor(x: number, y: number, width: number, height: number) {
+        super();
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+    }
+
+    show(): void {
+        console.log(`Ellipse:\n- Coordinates: ${this._x}, ${this._y}\n- Dimension: ${this._width} x ${this._height}\n`);
+    }
+    save() : string {
+        return `(${this._x},${this._y}),${this._width},${this._height}`;
+    }
+    load(data: string): void {
+        const parts = data.split(',');
+        this._x = Number(parts[0]);
+        this._y = Number(parts[1]);
+        this._width = Number(parts[2]);
+        this._height = Number(parts[3]);
+        console.log('Ellipse data has been loaded!\n');
+    }
+}
+
+const shapes: Shape[] = [
+    new Square(10, 10, 50),
+    new Rectangle(0, 0 , 100, 70),
+    new Circle(20, 30, 40),
+    new Ellipse(20, 20, 80, 60)
+]
+shapes.forEach((shape, index) => {
+    console.log(`Shape #${index + 1}: `);
+    shape.show();
+})
+
+let data: string = shapes[1].save();
+shapes[1].load("1, 2, 3, 4");
+shapes[1].show();
